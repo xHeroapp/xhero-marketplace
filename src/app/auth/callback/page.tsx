958f9ab8f7d659e95b38f884f4ba77b634dbe9b7
Joseph <ajogujoseph0317@gmail.com>
2025-12-11 01:05:56 +0100

    fixed bugs

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,6 +1,20 @@
-"use client";
 import AuthCallback from "@/components/auth/callback/AuthCallback";
+import { Suspense } from "react";
 
 export default function CallbackPage() {
-  return <AuthCallback />;
+  return (
+    <>
+      <Suspense
+        fallback={
+          <div className="login-wrapper d-flex align-items-center justify-content-center text-center">
+            <div className="spinner-border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          </div>
+        }
+      >
+        <AuthCallback />
+      </Suspense>
+    </>
+  );
 }
