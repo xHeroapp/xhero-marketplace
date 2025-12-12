791effb2ed44c786de87fdff534f8aed53a50690
Joseph <ajogujoseph0317@gmail.com>
2025-12-12 13:01:26 +0100

    merged

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,7 +8,7 @@ const index = () => {
         fallback={
           <div className="login-wrapper d-flex align-items-center justify-content-center text-center">
             <div className="spinner-border" role="status">
-              <span className="visually-hidden">Loading...</span>
+              <span className="visually-hidden">Loading..</span>
             </div>
           </div>
         }
