91d7d0b91e434ebb59c1188edf02c752861fc9dd
Melvin Senne-Aya <102502629+Mellocious@users.noreply.github.com>
2026-01-10 08:21:52 +0100

    Restructure product categories and fix store caching

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,8 +1,11 @@
 export const animationCreate = () => {
     if (typeof window !== "undefined") {
-        import("wowjs").then((module) => {
-            const WOW = module.default;
-            new WOW.WOW({ live: false }).init();
+        import("wowjs").then(() => {
+            // wowjs is a CommonJS module that attaches WOW to window
+            const WOW = window.WOW;
+            if (WOW) {
+                new WOW({ live: false }).init();
+            }
         });
     }
 };
