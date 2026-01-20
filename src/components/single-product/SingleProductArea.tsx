f0bcc5bcb6c08e450f808805d8be93c2f24dd3a8
Melvin Senne-Aya <102502629+Mellocious@users.noreply.github.com>
2026-01-20 19:41:47 +0100

    style: remove hardcoded product features list

--- src/components/single-product/SingleProductArea.tsx
+++ src/components/single-product/SingleProductArea.tsx
@@ -164,8 +164,8 @@ const SingleProductArea = ({ product }: any) => {
         <div className="product-title-meta-data bg-white mb-3 py-3">
           <div className="container d-flex justify-content-between rtl-flex-d-row-r">
             <div className="p-title-price">
-              <h5 className="mb-1"> {product.product_name}</h5>
-              <p className="sale-price mb-1 lh-1">
+              <h5 className="mb-2"> {product.product_name}</h5>
+              <p className="sale-price mb-2 lh-1">
                 {formatCurrency(product?.price)}
                 {/* <span> $ {product?.old_price ? product.old_price : "67"}</span> */}
               </p>
@@ -358,22 +358,7 @@ const SingleProductArea = ({ product }: any) => {
             <div className="product-description-content">
               <p>{product.long_description || "No description available"}</p>
             </div>
-            <ul className="mb-3 ps-3">
-              <li>
-                <i className="ti ti-check me-1"></i> 100% Good Reviews
-              </li>
-              <li>
-                <i className="ti ti-check me-1"></i> 7 Days Returns
-              </li>
-              <li>
-                {" "}
-                <i className="ti ti-check me-1"></i> Warranty not Aplicable
-              </li>
-              <li>
-                {" "}
-                <i className="ti ti-check me-1"></i> 100% Brand New Product
-              </li>
-            </ul>
+
           </div>
         </div>
 
